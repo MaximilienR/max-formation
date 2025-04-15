47e2e8dafb70e5019fab153d89822653e8b73529
maximilien <ro.maximilien@gmail.com>
2025-04-15 12:38:55 +0200

    update login

---
+++ src/components/Providers/FormProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import { FormContext } from "../../context/formContext";
+import { set } from "react-hook-form";
+
+
+function FormProvider({ children }) {
+
+  const [user, setUser] = useState(null);
+
+
+  const login = (data) => {
+    console.log(data);
+    setUser(data);
+  }
+  const logout = () => {
+    setUser(null);
+  }
+  return (
+    <FormContext.Provider value={{ user, login, logout }}>
+      {children}
+    </FormContext.Provider>
+  )
+}
+
+export default FormProvider ;
