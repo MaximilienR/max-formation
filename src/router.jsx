e6eb6a5a11db33ec1d3bc5c05e611fb58cf32e44
maximilien <ro.maximilien@gmail.com>
2025-05-09 15:18:09 +0200

    update btn

---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import APP from "./App";
+import Error from "./components/404/Error";
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        errorElement:<Error />,
+        element: <APP />,
+    },
+]);
