146009b34a91a8886411f21b22449125fe8f5b6a
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2025-05-14 09:55:23 +0200

    test debug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,15 +5,15 @@ function Home() {
   return (
     <div className="container p-4 mx-auto">
       <header>
-        <h1 className="mb-4 text-5xl font-bold text-center">L'aventure commence ici</h1>
-        <img src="/src/Assets/dev.jpg" alt="Image" className="object-cover w-full mb-4 h-1/2" />
+        <h1 className="mb-4 text-5xl font-bold text-center">L'aventureeeeeeeeeeeeeeeeeeeeeeeee commence ici</h1>
+        <img src="../src/Assets/dev.jpg" alt="Image" className="object-cover w-full mb-4 h-1/2" />
       </header>
       <main>
         <div className="flex flex-wrap justify-center">
           <section className="w-full p-4 md:w-1/2 xl:w-1/2">
             <h2 className="mb-2 text-3xl font-bold">Cours gratuit</h2>
             <Link to="/cours">
-              <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded">
+              <button className="px-4 py-2 font-bold text-white bg-yellow-400 rounded hover:bg-yellow-500">
                 Découvrir les cours
               </button>
             </Link>
@@ -25,7 +25,7 @@ function Home() {
           </section>
           <section className="w-full p-4 md:w-1/2 xl:w-1/2">
             <h2 className="mb-2 text-3xl font-bold">Coatching personnalisé</h2>
-            <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded">
+            <button className="px-4 py-2 font-bold text-white bg-yellow-400 rounded hover:bg-yellow-500">
               Découvrir le coaching
             </button>
             <ul>
