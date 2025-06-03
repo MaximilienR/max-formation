67bfddf94da055580cdbb5bd3c0a3ec1938e52bd
maximilien <ro.maximilien@gmail.com>
2025-06-03 16:11:36 +0200

    update certif

--- src/components/Certificat/Certificat.jsx
+++ src/components/Certificat/Certificat.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../../Assets/logo.png";
 
 export default function Certificat() {
   return (
@@ -7,30 +8,33 @@ export default function Certificat() {
         <h1 className="text-4xl font-extrabold text-yellow-500 mb-6 uppercase">
           Certificat de Réussite
         </h1>
-
-        <p className="text-lg mb-8text-amber-50 ">
+        <p className="text-lg mb-8 text-amber-50">
           Ce certificat est décerné à
         </p>
-
         <h2 className="text-3xl font-bold text-sky-900 mb-4 underline">
           [Nom de l'étudiant]
         </h2>
-
-        <p className="text-amber-50  text-lg mb-10">
+        <p className="text-amber-50 text-lg mb-10">
           Pour avoir complété avec succès la formation{" "}
-          <span className="font-semibold text-sky-800">
+          <span className="font-semibold text-orange-500">
             "[Nom de la formation]"
           </span>
           , démontrant engagement et excellence.
         </p>
-
-        <div className="flex justify-between items-center mt-8 px-6 text-gray-600 text-sm">
-          <div>
-            <p>Fait le : 03 juin 2025</p>
+        <div className="flex justify-between items-center mt-8 px-6 text-sm">
+          <div className="text-orange-500 text-base">
+            <p>
+              <span className="font-medium">Fait le :</span> 03 juin 2025
+            </p>
           </div>
-          <div className="text-right">
+          <div className="text-right text-gray-600">
             <p className="font-semibold">Signature</p>
             <div className="w-32 h-0.5 bg-gray-400 mx-auto mt-2"></div>
+            <img
+              src={logo}
+              alt="Logo de l'entreprise"
+              className="h-20 mx-auto mb-6"
+            />
           </div>
         </div>
       </div>
