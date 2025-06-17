1c539da08dbe416b72ea330f6fe494ef4664627c
maximilien <ro.maximilien@gmail.com>
2025-06-17 10:54:43 +0200

    update data json

--- src/pages/Achat.jsx
+++ src/pages/Achat.jsx
@@ -1,44 +1,42 @@
 import React from "react";
 import Aside from "../components/Profil/aside";
-
+import achat from "../data/achat.json";
 function Achat() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-1">
         <Aside className="w-1/6 bg-yellow-400 p-4" />
         <div className="flex-1">
-          <table class="table-auto border-collapse border border-gray-300 w-full">
+          <table className="table-auto border-collapse border border-gray-300 w-full">
             <thead>
-              <tr class="bg-gray-200">
-                <th class="border border-gray-300 px-4 py-2">
+              <tr className="bg-sky-900 text-white">
+                <th className="border border-gray-300 px-4 py-2">
                   Nom de l'article
                 </th>
-                <th class="border border-gray-300 px-4 py-2">Date</th>
-                <th class="border border-gray-300 px-4 py-2">Lien</th>
+                <th className="border border-gray-300 px-4 py-2">Date</th>
+                <th className="border border-gray-300 px-4 py-2">Lien</th>
               </tr>
             </thead>
             <tbody>
-              <tr class="hover:bg-gray-100">
-                <td class="border border-gray-300 px-4 py-2">HTML</td>
-                <td class="border border-gray-300 px-4 py-2">25.02.25</td>
-                <td class="border border-gray-300 px-4 py-2">
-                  <a href="">...</a>
-                </td>{" "}
-              </tr>
-              <tr class="hover:bg-gray-100">
-                <td class="border border-gray-300 px-4 py-2">CSS</td>
-                <td class="border border-gray-300 px-4 py-2">30.03.25</td>
-                <td class="border border-gray-300 px-4 py-2">
-                  <a href="">...</a>
-                </td>{" "}
-              </tr>
-              <tr class="hover:bg-gray-100">
-                <td class="border border-gray-300 px-4 py-2">JS</td>
-                <td class="border border-gray-300 px-4 py-2">28.04.25</td>
-                <td class="border border-gray-300 px-4 py-2">
-                  <a href="">...</a>
-                </td>
-              </tr>
+              {achat.map((item) => (
+                <tr key={item.id} className="hover:bg-gray-100">
+                  <td className="border border-gray-300 px-4 py-2">
+                    {item.nom}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {item.date}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    <a
+                      href={item.lien}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Voir
+                    </a>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
